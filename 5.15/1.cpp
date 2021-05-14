d51a4f6d0c7e981cae59b18cd46fa7bdee22d9cb
Rien_190 <1303086213@qq.com>
2021-05-14 23:29:42 +0800

    5.15train

---
+++ 5.15/1.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 朱睿涵 on 2021/5/14.
+//
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    printf("Welcome to The First Programming Competition of Hebei University of technology!");
+    return 0;
+}
